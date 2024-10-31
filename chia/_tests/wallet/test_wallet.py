9a868d9745652feb582fd700e3a1a0c0fbf35f6e
Matt <quexington@gmail.com>
2024-10-31 09:50:37 -0700

    Fix chia/_tests/wallet/test_wallet.py

--- chia/_tests/wallet/test_wallet.py
+++ chia/_tests/wallet/test_wallet.py
@@ -393,7 +393,7 @@ class TestWalletSimulator:
         assert len(txs["transactions"]) == 1
         assert not txs["transactions"][0]["confirmed"]
         assert txs["transactions"][0]["metadata"]["recipient_puzzle_hash"][2:] == normal_puzhash.hex()
-        assert txs["transactions"][0]["metadata"]["coin_id"] == merkle_coin.name().hex()
+        assert txs["transactions"][0]["metadata"]["coin_id"][2:] == merkle_coin.name().hex()
         with pytest.raises(ValueError):
             await api_0.spend_clawback_coins({})
 
@@ -577,7 +577,7 @@ class TestWalletSimulator:
         assert txs["transactions"][0]["confirmed"]
         assert txs["transactions"][1]["confirmed"]
         assert txs["transactions"][0]["memos"] != txs["transactions"][1]["memos"]
-        assert list(txs["transactions"][0]["memos"].values())[0] == b"Test".hex()
+        assert list(txs["transactions"][0]["memos"].values())[0][2:] == b"Test".hex()
 
     @pytest.mark.parametrize(
         "wallet_environments",
